942437d3c010e3a2ea0325ade112b038a576e3f3
mordecai9 <kolowskij@si.edu>
2018-11-28 17:15:32 -0500

    Reworking summary script, starting with alldata together

--- scripts/GridSummaryStats.R
+++ scripts/GridSummaryStats.R
@@ -1,48 +1,82 @@
 #This file calculates various summary statistics for the SCBI Camera Grid project. It should call in the clean data object called "camdataAllClean" from the data folder in the git repository
 
+library(reshape)
+library(dplyr)
+library(tidyverse)
+load("data/CamdataAllClean.RData")
 
+alldata <- camdataMSeq
+#Calculate Average, Minimum, and Maximum Capture Rate Across Grid, Across Seasons
 
-#########################################################################
-#Calculate Average, Minimum, and Maximum Number of Sequences Across Grid
-#########################################################################
+#Here I should remove unwanted species and categories right away.
+badSP <- c("Animal Not on List", "Camera Misfire", "Camera Trapper", "Corvus brachyrhynchos", "Corvus corax", "Cyanocitta cristata", "Homo sapiens", "Meleagris gallopavo", "No Animal", "Other Bird species", "Owl species", "Raptor Species", "Time Lapse", "Unknown Animal", "Unknown Bird", "Unknown Canid", "Unknown Skunk_Badger")
+alldata <- subset(alldata, !alldata$Species.Name %in% badSP)
+levels(as.factor(alldata$Species.Name))
+
 #create summary table of frequencies of sequences and convert it to a data frame
-camdata_summarym_SI17<-table(camdata_summaryd_SI17$Deployment_Name, 
-                             camdata_summaryd_SI17$Species.Name)
-camdata_summarym_SI17<-as.data.frame(camdata_summarym_SI17)
+camdata_summary<-table(alldata$Deployment_Name, 
+                             alldata$Species.Name)
+camdata_summary<-as.data.frame(camdata_summary)
 
 #change column names
-colnames(camdata_summarym_SI17)<-c("Deployment_Name", "Species", "Number_of_Sequences")
+colnames(camdata_summary)<-c("Deployment_Name", "Species", "Number_of_Sequences")
 
+#get unique camera effort values from sequence data for each deployment
+effort <- camdataMSeq %>%
+  select(Deployment_Name, Deploy.Duration) %>%
+  distinct
 
-#calculating average number of sequences for each species across the grid
-#This can be seen as a baseline sequence number against which we can compare sequence numbers at individual cameras
-baselines_SI17<- tapply(camdata_summarym_SI17$Number_of_Sequences, camdata_summarym_SI17$Species, mean)
-baselines_SI17<- as.data.frame(baselines_SI17)
-names(baselines_SI17)[1] <- "Mean_Seq"
+#Merge in the camera effort and calculate capture rate for each species and each deployment, as rate of sequences per 100 camera nights
+camdata_summary <- merge(camdata_summary, effort, by = "Deployment_Name" )
+camdata_summary$Deploy.Duration <- as.numeric(camdata_summary$Deploy.Duration)
+camdata_summary$CR <- (camdata_summary$Number_of_Sequences/camdata_summary$Deploy.Duration) *100
+
+#Shift table so species are along top and values are # of sequences, fill in blanks with zeros.Not sure if I need this format, but it is here just in case.
+camdata_summary_seqs<- spread(
+  data = camdata_summary,
+  key = Species,
+  value = Number_of_Sequences,
+  fill = 0
+)
+
+#Shift table so species are along top and values are capture rates, fill in blanks with zeros. Not sure if I need this format, but it is here just in case.
+camdata_summary_caprate<- spread(
+  data = camdata_summary,
+  key = Species,
+  value = CR,
+  fill = 0
+)
+#Remove unwanted columns from both files
+camdata_summary_caprate <- dplyr::select(camdata_summary_caprate, -("Number_of_Sequences"))
+camdata_summary_seqs <- dplyr::select(camdata_summary_seqs, -("CR"))
+
+#calculating average number of sequences for each species across the grid. This can be seen as a baseline sequence number against which we can compare sequence numbers at individual cameras. Note that in this case, this is across all 4 seasons, so we are averaging over more than 100 data points. It is now listing as 102 when I use length as the function in tapply. Should be higher no?
+
+baselines<- tapply(camdata_summary$CR, camdata_summary$Species, mean)
+baselines<- as.data.frame(baselines)
+names(baselines)[1] <- "Mean_CR"
 
 #SD of average number of sequences across the grid for each species
-baselines_sd_SI17 <- tapply(camdata_summarym_SI17$Number_of_Sequences, camdata_summarym_SI17$Species, sd)
-baselines_SI17$sd<- baselines_sd_SI17
-names(baselines_SI17)[2] <- "StDev_Seq"
+baselines_sd <- tapply(camdata_summary$CR, camdata_summary$Species, sd)
+baselines$sd<- baselines_sd
+names(baselines)[2] <- "StDev_CR"
 
 #number of sequences by species by camera, then transposing to have species as rows
-CR_bD_bS_SI17 <- tapply(camdata_summarym_SI17$Number_of_Sequences, list(camdata_summarym_SI17$Species, camdata_summarym_SI17$Deployment_Name), mean)
-CR_bD_bS_SI17 <- as.data.frame(CR_bD_bS_SI17)
+CR_bD_bS <- tapply(camdata_summary$CR, list(camdata_summary$Species, camdata_summary$Deployment_Name), mean)
+CR_bD_bS <- as.data.frame(CR_bD_bS)
 
-#Min and max of number of sequences for each species across the grid
-baselines_SI17$min <- apply(CR_bD_bS_SI17,1,min)
-names(baselines_SI17)[3]<- "Min_Seq"
-baselines_SI17$max <- apply(CR_bD_bS_SI17,1,max)
-names(baselines_SI17)[4]<-"Max_Seq"
+#Min and max CR for each species across the grid
+baselines$minCR <- apply(CR_bD_bS,1,min)
+names(baselines)[3]<- "Min_CR"
+baselines$maxCR <- apply(CR_bD_bS,1,max)
+names(baselines)[4]<-"Max_CR"
 
 #Add columns to CR_bD_bS for standard deviation, mean, max and min.
-CR_bD_bS_SI17$Mean_Seq <- baselines_SI17$Mean_Seq
-CR_bD_bS_SI17$StDev_Seq<-baselines_SI17$StDev_Seq
-CR_bD_bS_SI17$Min_Seq<-baselines_SI17$Min_Seq
-CR_bD_bS_SI17$Max_Seq<-baselines_SI17$Max_Seq
+CR_bD_bS$Mean_CR <- baselines$Mean_CR
+CR_bD_bS$StDev_CR<-baselines$StDev_CR
+CR_bD_bS$Min_CR<-baselines$Min_CR
+CR_bD_bS$Max_CR<-baselines$Max_CR
 
-#remove humans,birds,and misfires
-CR_bD_bS1_SI17<-CR_bD_bS_SI17[-c(1,2,7,9,11,16:21), ]
 
 
 #############################################################
