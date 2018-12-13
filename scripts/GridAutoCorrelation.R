4bbadd3ac0562773fc533ed5f7d2ab8079983409
mordecai9 <kolowskij@si.edu>
2018-12-13 18:39:08 -0500

    new script just for autocorrelation calcs and figures

---
+++ scripts/GridAutoCorrelation.R
@@ -0,0 +1,176 @@
+
+load("data/camdata_summary")
+
+#Create species by season files for focal species. Think about creating these in RegressionPrep file and using them here? May have to create them again there, and I need to pool squirrels here, like I do there.
+#Deer
+deerSumCR <- camdata_summary %>%
+  filter(Species == "Odocoileus virginianus" & Season == "Summer 2017") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+deerWinCR <- camdata_summary %>%
+  filter(Species == "Odocoileus virginianus" & Season == "Winter 2017") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+deerSprCR <- camdata_summary %>%
+  filter(Species == "Odocoileus virginianus" & Season == "Spring 2018") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+deerFallCR <- camdata_summary %>%
+  filter(Species == "Odocoileus virginianus" & Season == "Fall 2017") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+#Bear
+bearSumCR <- camdata_summary %>%
+  filter(Species == "Ursus americanus" & Season == "Summer 2017") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+bearWinCR <- camdata_summary %>%
+  filter(Species == "Ursus americanus" & Season == "Winter 2017") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+bearSprCR <- camdata_summary %>%
+  filter(Species == "Ursus americanus" & Season == "Spring 2018") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+bearFallCR <- camdata_summary %>%
+  filter(Species == "Ursus americanus" & Season == "Fall 2017") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+#raccoon
+raccoonSumCR <- camdata_summary %>%
+  filter(Species == "Procyon lotor" & Season == "Summer 2017") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+raccoonWinCR <- camdata_summary %>%
+  filter(Species == "Procyon lotor" & Season == "Winter 2017") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+raccoonSprCR <- camdata_summary %>%
+  filter(Species == "Procyon lotor" & Season == "Spring 2018") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+raccoonFallCR <- camdata_summary %>%
+  filter(Species == "Procyon lotor" & Season == "Fall 2017") %>%
+  dplyr::select(c("Deployment_Name2", "CR","NAD83_X", "NAD83_Y"))
+
+################################################
+#Calculating Moran's I - Spatial Autocorrelation
+################################################
+library(ape) #for Moran's I
+library(geoR) #for variograms
+
+#Generate distance matrix, one per season because of differing # of active points
+#Then take inverse of distance matrix and fill diagonals with 0
+distMatS<-as.matrix(dist(cbind(deerSumCR$NAD83_X, deerSumCR$NAD83_Y)))
+invDistMatS <- 1/distMatS
+diag(invDistMatS)<-0
+
+distMatF<-as.matrix(dist(cbind(deerFallCR$NAD83_X, deerFallCR$NAD83_Y)))
+invDistMatF <- 1/distMatF
+diag(invDistMatF)<-0
+
+distMatW<-as.matrix(dist(cbind(deerWinCR$NAD83_X, deerWinCR$NAD83_Y)))
+invDistMatW <- 1/distMatW
+diag(invDistMatW)<-0
+
+distMatSp<-as.matrix(dist(cbind(deerSprCR$NAD83_X, deerSprCR$NAD83_Y)))
+invDistMatSp <- 1/distMatSp
+diag(invDistMatSp)<-0
+
+
+#Use coords to create XY matrix
+XYmatrixS<-as.matrix(deerSumCR[,c(3,4)])
+XYmatrixF <- as.matrix(deerFallCR[,c(3,4)])
+XYmatrixSp <- as.matrix(deerSprCR[,c(3,4)])
+XYmatrixW <- as.matrix(deerWinCR[,c(3,4)])
+
+#Deer Analysis for Autocorrelation####
+#Deer_MI_inv[1:5, 1:5] #Need to ask Josey why she did this
+
+#Calculate Moran's I for Deer for all Seasons
+deerSumMoran<-Moran.I(deerSumCR$CR, invDistMatS)
+deerFallMoran<-Moran.I(deerFallCR$CR, invDistMatF)
+deerWinMoran<-Moran.I(deerWinCR$CR, invDistMatW)
+deerSprMoran<-Moran.I(deerSprCR$CR, invDistMatSp)
+
+#Deer Variograms for 4 Seasons. Need to remove the cameras with no data from Fall and Winter to have it work
+#Need to read more about setting the right bin size and how far we can interpret teh variogram (perhaps only half the field size?)
+par(mfrow = c(2,2), mar = c(5,5,5,5))
+DvarioS<-variog(coords = XYmatrixS, data = deerSumCR$CR, breaks = seq(0, 150,15), 
+               option = "bin")
+plot(DvarioS, xlim = c(0,120), ylim = c(0,15000), main = "Summer")
+
+DvarioW<-variog(coords = XYmatrixW, data = deerWinCR$CR, breaks = seq(0, 150,15), 
+                option = "bin")
+plot(DvarioW, xlim = c(0,120), ylim = c(0,15000), main = "Winter")
+
+DvarioSp<-variog(coords = XYmatrixSp, data = deerSprCR$CR, breaks = seq(0, 150,15), 
+                option = "bin")
+plot(DvarioSp, xlim = c(0,120), ylim = c(0,15000), main = "Spring")
+
+DvarioF<-variog(coords = XYmatrixF, data = deerFallCR$CR, breaks = seq(0, 150,15), 
+                option = "bin")
+plot(DvarioF, xlim = c(0,120), ylim = c(0,15000), main = "Fall")
+
+#################################
+#Raccoon Analysis####
+#Calculate Moran's I for bear for all Seasons
+raccoonSumMoran<-Moran.I(raccoonSumCR$CR, invDistMatS)
+raccoonFallMoran<-Moran.I(raccoonFallCR$CR, invDistMatF)
+raccoonWinMoran<-Moran.I(raccoonWinCR$CR, invDistMatW)
+raccoonSprMoran<-Moran.I(raccoonSprCR$CR, invDistMatSp)
+
+#raccoon Variograms for 4 Seasons. Need to remove the cameras with no data from Fall and Winter to have it work
+#Need to read more about setting the right bin size and how far we can interpret teh variogram (perhaps only half the field size?)
+par(mfrow = c(2,2), mar = c(5,5,5,5))
+RvarioS<-variog(coords = XYmatrixS, data = raccoonSumCR$CR, breaks = seq(0, 150,15), 
+                option = "bin")
+plot(RvarioS, xlim = c(0,120), ylim = c(0,2000), main = "Summer")
+
+RvarioW<-variog(coords = XYmatrixW, data = raccoonWinCR$CR, breaks = seq(0, 150,15), 
+                option = "bin")
+plot(RvarioW, xlim = c(0,120), ylim = c(0,2000), main = "Winter")
+
+RvarioSp<-variog(coords = XYmatrixSp, data = raccoonSprCR$CR, breaks = seq(0, 150,15), 
+                 option = "bin")
+plot(RvarioSp, xlim = c(0,120), ylim = c(0,2000), main = "Spring")
+
+RvarioF<-variog(coords = XYmatrixF, data = raccoonFallCR$CR, breaks = seq(0, 150,15), 
+                option = "bin")
+plot(RvarioF, xlim = c(0,120), ylim = c(0,2000), main = "Fall")
+
+#################################
+#Bear Analysis####
+#Calculate Moran's I for Raccoon for all Seasons
+bearSumMoran<-Moran.I(bearSumCR$CR, invDistMatS)
+bearFallMoran<-Moran.I(bearFallCR$CR, invDistMatF)
+bearWinMoran<-Moran.I(bearWinCR$CR, invDistMatW)
+bearSprMoran<-Moran.I(bearSprCR$CR, invDistMatSp)
+
+#bear Variograms for 4 Seasons. Need to remove the cameras with no data from Fall and Winter to have it work
+#Need to read more about setting the right bin size and how far we can interpret teh variogram (perhaps only half the field size?)
+par(mfrow = c(2,2), mar = c(5,5,5,5))
+BvarioS<-variog(coords = XYmatrixS, data = bearSumCR$CR, breaks = seq(0, 150,15), 
+                option = "bin")
+plot(BvarioS, xlim = c(0,120), ylim = c(0,300), main = "Summer")
+
+BvarioW<-variog(coords = XYmatrixW, data = bearWinCR$CR, breaks = seq(0, 150,15), 
+                option = "bin")
+plot(BvarioW, xlim = c(0,120), ylim = c(0,300), main = "Winter")
+
+BvarioSp<-variog(coords = XYmatrixSp, data = bearSprCR$CR, breaks = seq(0, 150,15), 
+                 option = "bin")
+plot(BvarioSp, xlim = c(0,120), ylim = c(0,300), main = "Spring")
+
+BvarioF<-variog(coords = XYmatrixF, data = bearFallCR$CR, breaks = seq(0, 150,15), 
+                option = "bin")
+plot(BvarioF, xlim = c(0,120), ylim = c(0,300), main = "Fall")
+
+
+
+#########################################
+#Plotting Covariance
+#########################################
+#Calculating Deer Covariance
+#Dcr = Deer_MI$Capture_Rate
+#Ddist<-as.data.frame(Deervario)
+#cov(Dcr, Ddist)
