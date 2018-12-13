3ff2e5fd9636bb9fbc41deeb77746fb67c7913e2
mordecai9 <kolowskij@si.edu>
2018-12-13 12:01:26 -0500

    corrected SE calcs, added comments, fixed merge with XY coords, added failed cams to point figures

--- scripts/GridSummaryStats.R
+++ scripts/GridSummaryStats.R
@@ -50,7 +50,7 @@ CRSeasonTots$SpringCR <- CRSeasonTots$SpringSeqs/seasonEffort$camNights[2]*100
 CRSeasonTots$SummerCR <- CRSeasonTots$SummerSeqs/seasonEffort$camNights[3]*100
 CRSeasonTots$WinterCR <- CRSeasonTots$WinterSeqs/seasonEffort$camNights[4]*100
 
-CRSummary <- merge(CRSeasonTots, CRAll, by = "row.names") #Final Season by Species summary of capture rates including overall pooled sequences capture rate over whole study
+CRSummary <- merge(CRSeasonTots, CRAll, by = "row.names") #Final Season by Species summary of capture rates including overall pooled sequences capture rate over whole study. Later in script I add in overall means across all seasons as well.
 
 #Merge in the camera effort and calculate capture rate for each species and each deployment, as rate of sequences per 100 camera nights
 camdata_summary <- merge(camdata_summary, effort, by = "Deployment_Name" )
@@ -58,6 +58,8 @@ camdata_summary$Deploy.Duration <- as.numeric(camdata_summary$Deploy.Duration)
 camdata_summary$CR <- (camdata_summary$Number_of_Sequences/camdata_summary$Deploy.Duration) *100
 camdata_summary$Season.y <- NULL
 names(camdata_summary)[4] <- "Season"
+
+save(camdata_summary, file = "data/camdata_summary")
 
 #Shift table so species are along top and values are # of sequences, fill in blanks with zeros.Not sure if I need this format, but it is here just in case.
 camdata_summary_seqs<- spread(
@@ -79,10 +81,10 @@ camdata_summary_caprate <- dplyr::select(camdata_summary_caprate, -("Number_of_S
 
 
 #____________________________________________________
-# Summary Stats - CapRates Overall and By Season ------------------------
+# Summary Stats - Mean CapRates Overall and By Season ------------------------
 #____________________________________________________
 
-#calculating average capture rate for each species across the grid. This can be seen as a baseline number against which we can compare numbers at individual cameras. Note that in this case, this is across all 4 seasons, so we are averaging over more than 100 data points. Can probably do this using summarize like I did for seasonal calcs
+#calculating average capture rate for each species across the grid. This can be seen as a baseline number against which we can compare numbers at individual cameras. Note that in this case, this is across all 4 seasons, so we are averaging over more than 100 data points. Can probably do this using summarize like I did for seasonal calcs. Note that these differ from the files above in that they are averages, not overall CR calculations.
 
 baselinesAll<- as.data.frame(tapply(camdata_summary$CR, camdata_summary$Species, mean))
 names(baselinesAll)[1] <- "Mean_CR"
@@ -95,17 +97,17 @@ names(baselinesAll)[2] <- "StDev_CR"
 #Capture rate by species by camera, here with species as rows
 CR_bD_bS <- as.data.frame(tapply(camdata_summary$CR, list(camdata_summary$Species, camdata_summary$Deployment_Name), mean))
 
-#Min and max CR for each species across the grid
+#Min and max CR for each species across the grid. I think I could have done this the same as I did with mean and sd above.
 baselinesAll$minCR <- apply(CR_bD_bS,1,min)
 names(baselinesAll)[3]<- "Min_CR"
 baselinesAll$maxCR <- apply(CR_bD_bS,1,max)
 names(baselinesAll)[4]<-"Max_CR"
 
 #Add columns to CR_bD_bS for standard deviation, mean, max and min.
-CR_bD_bS$Mean_CR <- baselinesAll$Mean_CR
-CR_bD_bS$StDev_CR<-baselinesAll$StDev_CR
-CR_bD_bS$Min_CR<-baselinesAll$Min_CR
-CR_bD_bS$Max_CR<-baselinesAll$Max_CR
+#CR_bD_bS$Mean_CR <- baselinesAll$Mean_CR
+#CR_bD_bS$StDev_CR<-baselinesAll$StDev_CR
+#CR_bD_bS$Min_CR<-baselinesAll$Min_CR
+#CR_bD_bS$Max_CR<-baselinesAll$Max_CR
 
 baselinesAll <- merge(baselinesAll, CRSummary, by.x = "row.names", by.y = "Row.names")
 
@@ -119,21 +121,40 @@ SeasonsCR <- camdata_summary %>%
             sdCR = sd(CR),
             minCR = min(CR),
             maxCR = max(CR),
-            seCR = sd(CR)/sqrt(27),
-            lowCI = mean(CR)-(1.96*(sd(CR)/sqrt(27))),
-            highCI = mean(CR)+(1.96*(sd(CR)/sqrt(27))),            
             DetCount = length(which(Number_of_Sequences >0)))
-SeasonsCR$lowCI <- ifelse(SeasonsCR$lowCI >= 0, SeasonsCR$lowCI, 0)
+            
+FWCalcs <- camdata_summary %>%
+  filter(Season == "Fall 2017" | Season == "Winter 2017") %>%
+  group_by(.dots = c("Season", "Species")) %>%
+     summarize(seCR = sd(CR)/sqrt(25),
+            lowCI = mean(CR)-(1.96*(sd(CR)/sqrt(25))),
+            highCI = mean(CR)+(1.96*(sd(CR)/sqrt(25))))            
+          
+SSCalcs <- camdata_summary %>%
+  filter(Season == "Summer 2017" | Season == "Spring 2018") %>%
+  group_by(.dots = c("Season", "Species")) %>%
+  summarize(seCR = sd(CR)/sqrt(27),
+            lowCI = mean(CR)-(1.96*(sd(CR)/sqrt(27))),
+            highCI = mean(CR)+(1.96*(sd(CR)/sqrt(27))))      
+
+NewCalcs <- rbind(FWCalcs, SSCalcs)
+SeasonsCR <- SeasonsCR %>%
+  left_join(NewCalcs, by = c("Season", "Species")) 
+  
+SeasonsCR$lowCI <- ifelse(SeasonsCR$lowCI >= 0, SeasonsCR$lowCI, 0) #make negative CI limits 0.
+
+
+
 
 #_______________________________
 #Calculate Proportion of Camera Deployments Confirming Each Species####
 #_______________________________
 
 #Create replicate data frame and remove mean,sd,min,max columns
-CR_bD_bS2<- dplyr::select(CR_bD_bS,-c("Mean_CR", "StDev_CR", "Min_CR", "Max_CR"))
+#CR_bD_bS2<- dplyr::select(CR_bD_bS,-c("Mean_CR", "StDev_CR", "Min_CR", "Max_CR"))
 
 #convert all values >0 to 1
-CR_bD_bS2<-as.data.frame((ifelse(CR_bD_bS2==0,0,1)))
+CR_bD_bS2<-as.data.frame((ifelse(CR_bD_bS==0,0,1)))
 
 #create column of camera capture proportion per species for whole grid to original data frame. SHOULD THINK ABOUT ADDING THIS INFO TO CR SUMMARY NOT CR_bD_bS
 CR_bD_bS$Prop_Grid<-rowSums(CR_bD_bS2)/length(unique(alldata$Deployment_Name))
@@ -141,8 +162,6 @@ CR_bD_bS$DetCount <- rowSums(CR_bD_bS2)
 CR_bD_bS$TotDepl <- length(unique(camdata_summary$Deployment_Name))
 
 #At this point I just have baseline summary values for capture rates by species, across all seasons, in a few difffert formats for various uses later.
-
-save(camdata_summary, file = "data/camdata_summary")
 
 #_________________________________________________________
 #Boxplot of CapRates and Proportions of Deployments----
@@ -166,28 +185,28 @@ labelsFall <- SeasonsCR %>%
   select(Species, Season, DetCount) %>%
   filter(Season == "Fall 2017") %>%
   mutate(FallDeployCt = 25) %>%
-  mutate(FallLabel = paste(Species,"|", DetCount, "/", FallDeployCt) ) %>%
+  mutate(FallLabel = paste(Species,"(", DetCount, "/", FallDeployCt, ")") ) %>%
   rename(FallDetCount = DetCount)  
 
 labelsSum <- SeasonsCR %>%
   select(Species, Season, DetCount) %>%
   filter(Season == "Summer 2017") %>%
   mutate(SumDeployCt = 27) %>%
-  mutate(SumLabel = paste(Species,"|", DetCount, "/", SumDeployCt) ) %>%
+  mutate(SumLabel = paste(Species,"(", DetCount, "/", SumDeployCt, ")") ) %>%
   rename(SumDetCount = DetCount) 
 
 labelsWinter <- SeasonsCR %>%
   select(Species, Season, DetCount) %>%
   filter(Season == "Winter 2017") %>%
   mutate(WinterDeployCt = 25) %>%
-  mutate(WinterLabel = paste(Species,"|", DetCount, "/", WinterDeployCt) ) %>%
+  mutate(WinterLabel = paste(Species,"(", DetCount, "/", WinterDeployCt, ")") ) %>%
   rename(WinDetCount = DetCount) 
 
 labelsSpring <- SeasonsCR %>%
   select(Species, Season, DetCount) %>%
   filter(Season == "Spring 2018") %>%
   mutate(SpringDeployCt = 27) %>%
-  mutate(SpringLabel = paste(Species,"|", DetCount, "/", SpringDeployCt) ) %>%
+  mutate(SpringLabel = paste(Species,"(", DetCount, "/", SpringDeployCt, ")") ) %>%
   rename(SprDetCount = DetCount) 
 
 #Merge proportion dataframe with rest of data
@@ -196,8 +215,8 @@ CR3<-merge(CR1, NumPres, by = "Species")
 CR3["tot"]<-length(unique(CR1$Deployment))
 
 #Adds column label which outputs the desired text labels for the boxplot. 
-#NEED TO MAKE LABELS FOR OTHER SEASONS HERE. I THINK I HAVE ALL IN NEED in CR3 to DO IT DIRECTLY
-CR3["labelAll"]<-paste(CR3$Species,"|", CR3$NumPres, "/", CR3$tot)
+
+CR3["labelAll"]<-paste(CR3$Species,"(", CR3$NumPres, "/", CR3$tot, ")")
 Labels_all <- CR3 %>%
   select(Species, labelAll) %>%
   distinct
@@ -229,6 +248,7 @@ mtext(expression(bold("Total Capture Rate (events per 100 camera-nights)")), sid
 #Good to go, just need to figure out how to order the species either the same every time, or in order by season.
 par(mfrow = c(2,2))
 par(mar=c(9,17,4,2))
+
 plot<-boxplot(camdata_summaryL$CR~camdata_summaryL$SumLabel,
               cex.main = 2.5,
               main = "",
@@ -237,7 +257,7 @@ plot<-boxplot(camdata_summaryL$CR~camdata_summaryL$SumLabel,
               horizontal = T, las = 2, cex.axis = 1,
               ylim = c(0,450),
               subset = camdata_summaryL$Season.x == "Summer 2017",
-              names.arg = camdata_summaryL$Species, at=rank(tapply(camdata_summaryL$CR,camdata_summaryL$Species,median), ties.method = "random"))
+              names.arg = camdata_summaryL$Species, at=rank(tapply(camdata_summaryL$CR,camdata_summaryL$Species, median), ties.method = "random"))
 mtext(expression(bold("Species")), side = 2, line = 15, cex = 1.7)
 mtext(expression(bold("Summer CR")), side = 1 , line = 4, cex = 1.3)
 
@@ -249,7 +269,7 @@ plot<-boxplot(data = camdata_summaryL, CR~FallLabel,
               horizontal = T, las = 2, cex.axis = 1,
               ylim = c(0,450),
               subset = Season.x == "Fall 2017",
-              names.arg = camdata_summaryL$Species, at=rank(tapply(camdata_summaryL$CR,camdata_summaryL$Species,median), ties.method = "random"))
+              names.arg = camdata_summaryL$Species, at=rank(tapply(camdata_summaryL$CR,camdata_summaryL$Species, median), ties.method = "random"))
 mtext(expression(bold("Fall CR")), side = 1 , line = 4, cex = 1.3)
 
 plot<-boxplot(data = camdata_summaryL, CR~WinterLabel,
@@ -260,7 +280,7 @@ plot<-boxplot(data = camdata_summaryL, CR~WinterLabel,
               horizontal = T, las = 2, cex.axis = 1,
               ylim = c(0,450),
               subset = Season.x == "Winter 2017",
-              names.arg = camdata_summaryL$Species, at=rank(tapply(camdata_summaryL$CR,camdata_summaryL$Species,median), ties.method = "random"))
+              names.arg = camdata_summaryL$Species, at=rank(tapply(camdata_summaryL$CR,camdata_summaryL$Species, median), ties.method = "random"))
 mtext(expression(bold("Species")), side = 2, line = 15, cex = 1.7)
 mtext(expression(bold("Winter CR")), side = 1 , line = 4, cex = 1.3)
 
@@ -272,7 +292,7 @@ plot<-boxplot(data = camdata_summaryL, CR~SpringLabel,
               horizontal = T, las = 2, cex.axis = 1,
               subset = Season.x == "Spring 2018",
               ylim = c(0,450),
-              names.arg = camdata_summaryL$Species, at=rank(tapply(camdata_summaryL$CR,camdata_summaryL$Species,median), ties.method = "random"))
+              names.arg = camdata_summaryL$Species, at=rank(tapply(camdata_summaryL$CR,camdata_summaryL$Species, median), ties.method = "random"))
 mtext(expression(bold("Spring CR")), side = 1 , line = 4, cex = 1.3)
 
 #______________________________________________
@@ -282,12 +302,24 @@ mtext(expression(bold("Spring CR")), side = 1 , line = 4, cex = 1.3)
 
 #Bring in XY data for grid
 gridXY <- read.csv("data/Grid_Coordinates.csv")
+#Create file with coding in each type for deployments
+deployCodes <- alldata %>%
+  select(Deployment_Name, Deployment_Name2) %>%
+  distinct
+#Add basic deployment code into camdata_summary
+camdata_summary <- merge(camdata_summary, deployCodes, by.x = "Deployment_Name")
+
 camdata_summary <- merge(camdata_summary, gridXY, by.x = "Deployment_Name2", by.y = "Deployment")
 
 #_____________________________________________
 ##CapRates Point Size Figures- DEER----
 #_____________________________________________
-#Would be nice to add proportion of cameras with hits, or to label cameras that had no detections. Would also be good to have a small key to circle size, maybe showing lower quartile, median and upper quartile values? Final figures will probably just have Season names, with the species in the caption?
+#Would be nice to add proportion of cameras with hits, or to label cameras that had no detections. Would also be good to have a small key to circle size, maybe showing lower quartile, median and upper quartile values? Final figures will probably just have Season names, with the species in the caption? Need to add symbol for failed cameras in Fall and Winter.
+
+#call out failed points for Fall deployments
+idxf <- which(gridXY$Deployment == "505" | gridXY$Deployment == "206")
+#call out failed points for Winter deployments
+idxw <- which(gridXY$Deployment == "101" | gridXY$Deployment == "401")
 
 par(mar = c(1,1,1,1))
 par(mfrow = c(2,2))
@@ -339,6 +371,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Deer - Fall", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxf], gridXY$NAD83_Y[idxf], cex = 1, pch = 8, col = "red")
 
 #DEER WINTER
 s=12
@@ -363,6 +396,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Deer - Winter", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxw], gridXY$NAD83_Y[idxw], cex = 1, pch = 8, col = "red")
 
 #DEER Spring
 s=12
@@ -442,6 +476,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Fox sq. - Fall", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxf], gridXY$NAD83_Y[idxf], cex = 1, pch = 8, col = "red")
 
 #Fox Squirrel Winter
 s=6
@@ -466,6 +501,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Fox Sq. - Winter", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxw], gridXY$NAD83_Y[idxw], cex = 1, pch = 8, col = "red")
 
 #Fox Squirrel - Spring
 s=6
@@ -544,6 +580,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Gray sq. - Fall", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxf], gridXY$NAD83_Y[idxf], cex = 1, pch = 8, col = "red")
 
 #Gray Squirrel Winter
 s=6
@@ -568,6 +605,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Gray sq. - Winter", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxw], gridXY$NAD83_Y[idxw], cex = 1, pch = 8, col = "red")
 
 #Gray Squirrel Spring
 s=6
@@ -647,6 +685,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Raccoon - Fall", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxf], gridXY$NAD83_Y[idxf], cex = 1, pch = 8, col = "red")
 
 #Raccoon Winter
 s=5
@@ -671,6 +710,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Raccoon - Winter", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxw], gridXY$NAD83_Y[idxw], cex = 1, pch = 8, col = "red")
 
 #Raccoon Spring
 s=5
@@ -749,6 +789,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Black Bear - Fall", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxf], gridXY$NAD83_Y[idxf], cex = 1, pch = 8, col = "red")
 
 #Black Bear Winter
 s=1
@@ -773,6 +814,7 @@ points(camdata_summary$NAD83_X, camdata_summary$NAD83_Y,
        xlim = c(747430, 747550),
        ylim = c(4308910,4309030))
 text(labels = "Black Bear - Winter", y = 4309025, x = 747430, cex = 1.6, pos = 4)
+points(gridXY$NAD83_X[idxw], gridXY$NAD83_Y[idxw], cex = 1, pch = 8, col = "red")
 
 #Black Bear Spring
 s=1
