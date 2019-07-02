f58373a9033775f314d5300b19e7b6389e3b0123
OleyJ <joley@masonlive.gmu.edu>
2019-07-02 14:50:57 -0400

    Update JaccardSimilarityIndex.R
    uploading the DetHist files by season

--- scripts/JaccardSimilarityIndex.R
+++ scripts/JaccardSimilarityIndex.R
@@ -2,7 +2,66 @@
 
 library(jaccard)
 
-#Load Files 
+##Load species files for Spring
+load("results/DetHistSpSciurus niger")
+DHSpSN <- temp
 
+load("results/DetHistSpSciurus carolinensis")
+DHSpSC <- temp
+
+load("results/DetHistSpOdocoileus virginianus")
+DHSpOV <- temp
+
+load("results/DetHistSpUrsus americanus")
+DHSpUA <- temp
+
+load("results/DetHistSpProcyon lotor")
+DHSpPL <- temp
+
+##Load species files for Summer
+load("results/DetHistSumSciurus niger")
+DHSumSN <- temp
+
+load("results/DetHistSumSciurus carolinensis")
+DHSumSC <- temp
+
+load("results/DetHistSumOdocoileus virginianus")
+DHSumOV <- temp
+
+load("results/DetHistSumUrsus americanus")
+DHSumUA <- temp
+
+load("results/DetHistSumProcyon lotor")
+DHSumPL <- temp
+
+##Load species for Fall
+load("results/DetHistFallSciurus niger")
+DHFallSN <- temp
+
+load("results/DetHistFallSciurus carolinensis")
+DHFallSC <- temp
+
+load("results/DetHistFallOdocoileus virginianus")
+DHFallOV <- temp
+
+load("results/DetHistFallUrsus americanus")
+DHFallUA <- temp
+
+load("results/DetHistFallProcyon lotor")
+DHFallPL <- temp
+
+##Load species for Winter
 load("results/DetHistWinSciurus niger")
 DHWinSN <- temp
+
+load("results/DetHistWinSciurus carolinensis")
+DHWinSC <- temp
+
+load("results/DetHistWinOdocoileus virginianus")
+DHWinOV <- temp
+
+load("results/DetHistWinUrsus americanus")
+DHWinUA <- temp
+
+load("results/DetHistWinProcyon lotor")
+DHWinPL <- temp
